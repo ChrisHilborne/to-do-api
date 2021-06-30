f171649cc1e51ce1e5cabdce0b5a11b16e7154df
ChrisHilborne <chris.hilborne@gmail.com>
2021-06-30 12:14:01 +0200

    Created TaskServiceImpl class and wrote failing test for getTakById() method

---
+++ src/main/java/com/chilborne/todoapi/service/TaskServiceImpl.java
@@ -0,0 +1,50 @@
+package com.chilborne.todoapi.service;
+
+import com.chilborne.todoapi.exception.DataNotFoundException;
+import com.chilborne.todoapi.exception.TaskAlreadyCompletedException;
+import com.chilborne.todoapi.exception.TaskNotFoundException;
+import com.chilborne.todoapi.persistance.model.Task;
+import com.chilborne.todoapi.persistance.repository.TaskRepository;
+import com.chilborne.todoapi.web.dto.SingleValueDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository repository;
+    private final Logger logger = LoggerFactory.getLogger(TaskServiceImpl.class);
+
+    public TaskServiceImpl(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Task getTaskById(long id) throws TaskNotFoundException {
+        return new Task("fails");
+    }
+
+    @Override
+    public Task saveTask(Task task) {
+        return null;
+    }
+
+    @Override
+    public Task setTaskName(long id, SingleValueDTO<String> name) throws TaskNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Task setTaskDescription(long id, SingleValueDTO<String> description) throws TaskNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Task setTaskToDoList(long taskId, long toDoListId) throws DataNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Task completeTask(long id) throws TaskNotFoundException, TaskAlreadyCompletedException {
+        return null;
+    }
+}
