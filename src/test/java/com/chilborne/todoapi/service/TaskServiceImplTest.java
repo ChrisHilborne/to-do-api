49653d0e20725981fe69823e888dc06a7000248f
ChrisHilborne <chris.hilborne@gmail.com>
2021-06-30 12:14:01 +0200

    Created TaskServiceImpl class and wrote failing test for getTakById() method

---
+++ src/test/java/com/chilborne/todoapi/service/TaskServiceImplTest.java
@@ -0,0 +1,75 @@
+package com.chilborne.todoapi.service;
+
+import com.chilborne.todoapi.persistance.model.Task;
+import com.chilborne.todoapi.persistance.model.ToDoList;
+import com.chilborne.todoapi.persistance.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceImplTest {
+
+    @Mock
+    TaskRepository repository;
+
+    @InjectMocks
+    TaskServiceImpl service;
+
+    @Captor
+    ArgumentCaptor<Task> taskCaptor;
+
+    Task testTask;
+
+    @BeforeEach
+    void init() {
+        ToDoList testList = new ToDoList("test list");
+        testTask = new Task(testList, "test task");
+        testTask.setTaskId(50L);
+    }
+
+    @Test
+    void getTaskByIdShouldReturnCorrectTaskWhenItExists() {
+        //given
+        given(repository.findById(50L)).willReturn(Optional.ofNullable(testTask));
+
+        //when
+        Task result = service.getTaskById(50L);
+
+        //verify
+        assertEquals(testTask, result);
+        verify(repository).findById(50L);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    void saveTask() {
+    }
+
+    @Test
+    void setTaskName() {
+    }
+
+    @Test
+    void setTaskDescription() {
+    }
+
+    @Test
+    void setTaskToDoList() {
+    }
+
+    @Test
+    void completeTask() {
+    }
+}
