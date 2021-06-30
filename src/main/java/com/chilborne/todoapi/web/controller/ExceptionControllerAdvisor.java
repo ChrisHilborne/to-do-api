c70af704e4cd9848118c9a87ac0e8ba7235fae64
ChrisHilborne <chris.hilborne@gmail.com>
2021-06-30 12:06:05 +0200

    Created TaskService interface

--- src/main/java/com/chilborne/todoapi/web/controller/ExceptionControllerAdvisor.java
+++ src/main/java/com/chilborne/todoapi/web/controller/ExceptionControllerAdvisor.java
@@ -1,6 +1,8 @@
-package com.chilborne.todoapi.web.error;
+package com.chilborne.todoapi.web.controller;
 
 import com.chilborne.todoapi.exception.DataNotFoundException;
+import com.chilborne.todoapi.exception.TaskAlreadyCompletedException;
+import com.chilborne.todoapi.web.error.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,7 +16,7 @@ public class ExceptionControllerAdvisor {
 
     private final Logger logger = LoggerFactory.getLogger(ExceptionControllerAdvisor.class);
 
-    @ExceptionHandler(DataNotFoundException.class)
+    @ExceptionHandler({DataNotFoundException.class, TaskAlreadyCompletedException.class})
     public ErrorResponse handleDataNotFoundException(HttpServletRequest req, Exception e) {
         logger.error("Request: " + req.getRequestURI() + " raised " + e);
         return new ErrorResponse(HttpStatus.NOT_FOUND, e);
