57c5668d2074b906934ca92fafc76bbb84280bd0
ChrisHilborne <chris.hilborne@gmail.com>
2021-07-22 12:17:13 +0200

    Added /persistence/mapper/UserMapper

--- src/main/java/com/chilborne/todoapi/persistance/mapper/UserMapper.java
+++ src/main/java/com/chilborne/todoapi/persistance/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package com.chilborne.todoapi.persistance.mapper;
 
-public class UserMapper {
+import com.chilborne.todoapi.persistance.dto.UserDto;
+import com.chilborne.todoapi.persistance.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper(componentModel = "string", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public abstract class UserMapper {
+
+    public abstract UserDto convertUser(User user);
+
+    public abstract User convertUserDto(UserDto dto);
 }
