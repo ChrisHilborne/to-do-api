36312dc3c58c6d5328d9ca49450cfa2171a49b1a
ChrisHilborne <chris.hilborne@gmail.com>
2021-07-25 19:31:39 +0200

    Set up bootstrap of User for testing

--- src/main/java/com/chilborne/todoapi/persistance/bootstrap/BootstrapDatabase.java
+++ src/main/java/com/chilborne/todoapi/persistance/bootstrap/BootstrapDatabase.java
@@ -2,7 +2,9 @@ package com.chilborne.todoapi.persistance.bootstrap;
 
 import com.chilborne.todoapi.persistance.model.Task;
 import com.chilborne.todoapi.persistance.model.ToDoList;
+import com.chilborne.todoapi.persistance.model.User;
 import com.chilborne.todoapi.persistance.repository.ToDoListRepository;
+import com.chilborne.todoapi.persistance.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -16,23 +18,32 @@ import java.util.List;
 @ActiveProfiles("!test")
 public class BootstrapDatabase {
 
-    private final ToDoListRepository repository;
+    private final ToDoListRepository toDoListRepository;
+    private final UserRepository userRepository;
     private final Logger logger = LoggerFactory.getLogger(BootstrapDatabase.class);
 
-    public BootstrapDatabase(ToDoListRepository repository) {
-        this.repository = repository;
+    public BootstrapDatabase(
+            ToDoListRepository toDoListRepository,
+            UserRepository userRepository) {
+        this.toDoListRepository = toDoListRepository;
+        this.userRepository = userRepository;
     }
 
     @Bean
     CommandLineRunner init() {
         return args -> {
             logger.debug("Bootstrapping data...");
+            User user = new User("user", "password", "email@test.com");
+
             ToDoList toDoList = new ToDoList("To Do");
             Task washingTask = new Task(toDoList, "Washing");
             Task cleaningTask = new Task(toDoList, "Cleaning");
+            toDoList.setUser(user);
             toDoList.setTasks(List.of(washingTask, cleaningTask));
 
-            repository.save(toDoList);
+            user.setToDoLists(List.of(toDoList));
+            userRepository.save(user);
+            toDoListRepository.save(toDoList);
             logger.debug("Data bootstrapped.");
         };
     }
