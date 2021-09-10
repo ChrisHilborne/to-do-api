e1515dcdf21440589307d280c22d787e26b11393
ChrisHilborne <chris.hilborne@gmail.com>
2021-09-10 23:45:40 +0200

    Configured CommonsRequestLoggingFilter bean to log incoming connections

---
+++ src/main/java/com/chilborne/todoapi/web/filter/CommonsRequestLoggingFilterConfig.java
@@ -0,0 +1,22 @@
+package com.chilborne.todoapi.web.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class CommonsRequestLoggingFilterConfig {
+
+  @Bean
+  public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
+    CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeClientInfo(true);
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setIncludeHeaders(true);
+    filter.setHeaderPredicate(h -> h.equals("user-agent"));
+
+    return filter;
+  }
+
+}
